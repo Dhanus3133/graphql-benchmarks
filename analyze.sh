73e3190f2e571ff329bf6ca37adefed0464d5126
Dhanus <dhanus3133@gmail.com>
2023-11-15 18:18:22 +0530

    formatting fixes

--- analyze.sh
+++ analyze.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function extractMetric() {
-    local file="$1"
-    local metric="$2"
-    grep "$metric" "$file" | awk '{print $2}' | sed 's/ms//'
+	local file="$1"
+	local metric="$2"
+	grep "$metric" "$file" | awk '{print $2}' | sed 's/ms//'
 }
 
 function average() {
-    echo "$@" | awk '{for(i=1;i<=NF;i++) s+=$i; print s/NF}'
+	echo "$@" | awk '{for(i=1;i<=NF;i++) s+=$i; printf "`%\047.2f`\n", s/NF}'
 }
 
 servers=("apollo" "netflixdgs" "gqlgen" "tailcall")
@@ -17,54 +17,53 @@ declare -A avgLatencies
 
 # Extract metrics and calculate averages
 for idx in "${!servers[@]}"; do
-    startIdx=$((idx * 3))
-    reqSecVals=()
-    latencyVals=()
-    for j in 0 1 2; do
-        fileIdx=$((startIdx + j))
-        reqSecVals+=($(extractMetric "${resultFiles[$fileIdx]}" "Requests/sec"))
-        latencyVals+=($(extractMetric "${resultFiles[$fileIdx]}" "Latency"))
-    done
-    avgReqSecs[${servers[$idx]}]=$(average "${reqSecVals[@]}")
-    avgLatencies[${servers[$idx]}]=$(average "${latencyVals[@]}")
+	startIdx=$((idx * 3))
+	reqSecVals=()
+	latencyVals=()
+	for j in 0 1 2; do
+		fileIdx=$((startIdx + j))
+		reqSecVals+=($(extractMetric "${resultFiles[$fileIdx]}" "Requests/sec"))
+		latencyVals+=($(extractMetric "${resultFiles[$fileIdx]}" "Latency"))
+	done
+	avgReqSecs[${servers[$idx]}]=$(average "${reqSecVals[@]}")
+	avgLatencies[${servers[$idx]}]=$(average "${latencyVals[@]}")
 done
 
 # Generating data files for gnuplot
 reqSecData="/tmp/reqSec.dat"
 latencyData="/tmp/latency.dat"
 
-echo "Server Value" > "$reqSecData"
+echo "Server Value" >"$reqSecData"
 for server in "${servers[@]}"; do
-    echo "$server ${avgReqSecs[$server]}" >> "$reqSecData"
+	echo "$server ${avgReqSecs[$server]}" >>"$reqSecData"
 done
 
-echo "Server Value" > "$latencyData"
+echo "Server Value" >"$latencyData"
 for server in "${servers[@]}"; do
-    echo "$server ${avgLatencies[$server]}" >> "$latencyData"
+	echo "$server ${avgLatencies[$server]}" >>"$latencyData"
 done
 
 # Plotting using gnuplot
-gnuplot <<- EOF
-    set term pngcairo size 1280,720 enhanced font "Courier,12"
-    set output "req_sec_histogram.png"
-    set style data histograms
-    set style histogram cluster gap 1
-    set style fill solid border -1
-    set xtics rotate by -45
-    set boxwidth 0.9
-    set title "Requests/Sec"
-    stats "$reqSecData" using 2 nooutput
-    set yrange [0:STATS_max*1.2]
-    set key outside right top
-    plot "$reqSecData" using 2:xtic(1) title "Req/Sec"
+gnuplot <<-EOF
+	    set term pngcairo size 1280,720 enhanced font "Courier,12"
+	    set output "req_sec_histogram.png"
+	    set style data histograms
+	    set style histogram cluster gap 1
+	    set style fill solid border -1
+	    set xtics rotate by -45
+	    set boxwidth 0.9
+	    set title "Requests/Sec"
+	    stats "$reqSecData" using 2 nooutput
+	    set yrange [0:STATS_max*1.2]
+	    set key outside right top
+	    plot "$reqSecData" using 2:xtic(1) title "Req/Sec"
 
-    set output "latency_histogram.png"
-    set title "Latency (in ms)"
-    stats "$latencyData" using 2 nooutput
-    set yrange [0:STATS_max*1.2]
-    plot "$latencyData" using 2:xtic(1) title "Latency"
+	    set output "latency_histogram.png"
+	    set title "Latency (in ms)"
+	    stats "$latencyData" using 2 nooutput
+	    set yrange [0:STATS_max*1.2]
+	    plot "$latencyData" using 2:xtic(1) title "Latency"
 EOF
-
 
 # Move PNGs to assets
 mkdir -p assets
@@ -75,18 +74,18 @@ mv latency_histogram.png assets/
 resultsTable="<!-- PERFORMANCE_RESULTS_START -->\n| Server | Requests/sec | Latency (ms) |\n|--------|--------------|--------------|"
 
 for server in "${servers[@]}"; do
-    resultsTable+="\n| $server | ${avgReqSecs[$server]} | ${avgLatencies[$server]} |"
+	resultsTable+="\n| $server | ${avgReqSecs[$server]} | ${avgLatencies[$server]} |"
 done
 
 resultsTable+="\n<!-- PERFORMANCE_RESULTS_END -->"
 
 # Check if the markers are present
 if grep -q "PERFORMANCE_RESULTS_START" README.md; then
-    # Replace the old results with the new results
-    sed -i "/PERFORMANCE_RESULTS_START/,/PERFORMANCE_RESULTS_END/c\\$resultsTable" README.md
+	# Replace the old results with the new results
+	sed -i "/PERFORMANCE_RESULTS_START/,/PERFORMANCE_RESULTS_END/c\\$resultsTable" README.md
 else
-    # Append the results at the end of the README.md file
-    echo -e "\n$resultsTable" >> README.md
+	# Append the results at the end of the README.md file
+	echo -e "\n$resultsTable" >>README.md
 fi
 
 # Print the results as a table in the terminal
@@ -103,5 +102,5 @@ git push
 
 # Delete the result TXT files
 for file in "${resultFiles[@]}"; do
-    rm "$file"
+	rm "$file"
 done
