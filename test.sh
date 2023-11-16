2d9952dd897b3faadce1d072f46f165049c050ed
Dhanus <dhanus3133@gmail.com>
2023-11-16 18:12:20 +0530

    okay final

--- test.sh
+++ test.sh
@@ -11,13 +11,13 @@ result=$(average $numbers)
 
 # Format numbers with commas
 # formattedNumbers=$(echo $numbers | sed 's/\([[:digit:]]\{3\}\)\([[:digit:]]\{3\}\)\([[:digit:]]\{3\}\)/\1,\2,\3/g')
-# echo 123456789 | awk '$0=gensub(/(...)/,"\\1,","g")'
+echo -e 123456789 | awk '$0=gensub(/(...)/,"\\1,","g")' >README2.md
 # echo 123456.789 | awk '{printf ("%'\''d\n", $0)}'
 
 # echo "Numbers: $numbers"
 # echo "Average: $formattedNumbers"
-echo "Input: " $numbers
-echo "Average: " $result
+# echo "Input: " $numbers
+# echo "Average: " $result
 
 # Write output to README2.md
 # echo -e "Numbers: 182,928.12 $formattedNumbers\\nAverage: $result" >README2.md
