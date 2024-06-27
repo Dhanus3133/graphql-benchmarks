fb3123589f87fa75ecd84f77e746182f1350208f
Dhanus <dhanus3133@gmail.com>
2024-06-27 14:04:59 +0530

    committed changes

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # For gqlgen:
-cd graphql/gqlgen
-go build -o main main.go
-cd ../../
-# For apollo server:
-cd graphql/apollo_server
-npm i
-cd ../../
-
-# For netflix dgs
-cd graphql/netflix_dgs
-./gradlew build
-cd ../../
-
-# For tailcall:
-cd graphql/tailcall
-npm install
-cd ../../
-
-# For caliban
-cd graphql/caliban
-./sbt compile
-cd ../../
-
-# For async-graphql
-./graphql/async_graphql/build.sh
-cd ../../
+# cd graphql/gqlgen
+# go build -o main main.go
+# cd ../../
+# # For apollo server:
+# cd graphql/apollo_server
+# npm i
+# cd ../../
+#
+# # For netflix dgs
+# cd graphql/netflix_dgs
+# ./gradlew build
+# cd ../../
+#
+# # For tailcall:
+# cd graphql/tailcall
+# npm install
+# cd ../../
+#
+# # For caliban
+# cd graphql/caliban
+# ./sbt compile
+# cd ../../
+#
+# # For async-graphql
+# ./graphql/async_graphql/build.sh
+# cd ../../
 
 # For hasura
 cd ./graphql/hasura
